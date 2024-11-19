a41cd12ec44b213a04b2c231b5f39b3079dedb9b
rodrigocitadin <citadinrodrigo@gmail.com>
2024-11-19 17:43:47 -0300

    improve clumsy code and add echo route

--- app/server.go
+++ app/server.go
@@ -7,9 +7,6 @@ import (
 	"strings"
 )
 
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -19,20 +16,42 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
+}
 
-	req := make([]byte, 1024)
-	conn.Read(req)
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
 
-	if !strings.HasPrefix(string(req), "GET / HTTP/1.1") {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		conn.Close()
+	reqBuffer := make([]byte, 1024)
+
+	n, err := conn.Read(reqBuffer)
+	if err != nil {
+		fmt.Println("Failed to read the request:", err)
 		return
 	}
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	fmt.Printf("Request: %s\n", reqBuffer[:n])
+
+	req := string(reqBuffer[:n])
+	path := strings.Split(req, " ")[1]
+
+	switch {
+	case path == "/":
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	case strings.Split(path, "/")[1] == "echo":
+		message := strings.Split(path, "/")[2]
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
+	default:
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
 }
