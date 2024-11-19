d84be254e2c923c32f4eb6414c5fbfa881b68374
rodrigocitadin <citadinrodrigo@gmail.com>
2024-11-19 00:07:09 -0300

    extracting URL path

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 var _ = net.Listen
@@ -22,6 +23,15 @@ func main() {
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+
+	req := make([]byte, 1024)
+	conn.Read(req)
+
+	if !strings.HasPrefix(string(req), "GET / HTTP/1.1") {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		conn.Close()
+		return
 	}
 
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
